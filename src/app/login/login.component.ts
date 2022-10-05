e24f64b07c180542ba82c44ff510ecc606c66510
Eliebm <eliebm17@live.com>
2022-10-05 16:30:51 +0300

    validation process

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl,Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'pm-login',
@@ -6,12 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    public first_name : string='';
+
+   loginform= new FormGroup({
+    fname:new FormControl("",[Validators.required]),
+    lname:new FormControl(''),
+
+   });
 
   constructor() { }
 
   ngOnInit(): void {
+   
   }
+
+  get fnamevalid(){
+
+    return this.loginform.get('fname');
+  }
+
+ 
+
   submit():void{
-    
+   console.log(this.first_name);
   }
 }
