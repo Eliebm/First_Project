53f9da9784df54a4f8ec27fa84a091c20b151c45
Eliebm <eliebm17@live.com>
2022-10-12 13:26:38 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignupComponent } from './signup/signup.component';
+
 
 const routes: Routes = [{ path:'', redirectTo:'Signup', pathMatch: 'full' },
 {path:'Signup', loadChildren:() => import('./signup/signup.module').then(m=>m.SignupModule)}
