e68162e9bc9d5ea4274f6533e173db995edadd46
Eliebm <eliebm17@live.com>
2022-10-18 12:49:13 +0300

    redesign

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-const routes: Routes = [{ path:'', redirectTo:'login', pathMatch: 'full' },
+const routes: Routes = [{ path:'', redirectTo:'Login', pathMatch: 'full' },
 {path:'Signup', loadChildren:() => import('./signup/signup.module').then(m=>m.SignupModule)},
-{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
+{ path: 'Login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
 ];
 
 @NgModule({
