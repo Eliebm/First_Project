7092d7a283163c559764a3d72d317e1bc62ecf7f
Eliebm <eliebm17@live.com>
2022-11-07 14:18:16 +0200

    bug fixes and improvements

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,24 +3,27 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ICountries } from '../account';
 import { CountriesDataService } from '../countries-data.service';
 import { Location } from '@angular/common';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'pm-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
+
 })
 export class DetailsComponent implements OnInit {
+  private _localUrl = environment.baseUrl;
   countryData: ICountries[] = [];
   countryName: any;
-  adminOptionList: any[] = [{ "name": "Home", "value": "http://localhost:4200/home" }, { "name": "Edit", "value": "" }, { "name": "Gallery ", "value": "" }];
-  userOptionList: any[] = [{ "name": "Home", "value": "http://localhost:4200/home" }, { "name": "Gallery ", "value": "" }];
+  adminOptionList: any[] = [{ "name": "Home", "value": this._localUrl + "home" }, { "name": "Edit", "value": "" }, { "name": "Gallery ", "value": "" }];
+  userOptionList: any[] = [{ "name": "Home", "value": this._localUrl + "home" }, { "name": "Gallery ", "value": "" }];
   optionList: any[] = [];
   sessionData: any;
   isEditable: boolean = false;
 
 
 
-  constructor(private countryDService: CountriesDataService, private route: ActivatedRoute, private location: Location) { }
+  constructor(private _countryDService: CountriesDataService, private _route: ActivatedRoute, private _location: Location) { }
 
   ngOnInit(): void {
 
@@ -31,25 +34,24 @@ export class DetailsComponent implements OnInit {
     } else { this.optionList = this.adminOptionList.map(item => item); }
 
 
-    this.countryName = this.route.snapshot.paramMap.get('countName');
+    this.countryName = this._route.snapshot.paramMap.get('countName');
     this.getAllDetails();
 
   }
 
   getAllDetails(): void {
 
-    this.countryDService.getCountryDetails(this.countryName).subscribe(data => this.countryData = data);
+    this._countryDService.getCountryDetails(this.countryName).subscribe(data => this.countryData = data);
 
 
   }
 
-  getBorderCountries(code: string): void {
-    this.countryDService.getCountryDetailsByCode(code).subscribe(data => this.countryData = data);
+  getDetailsOfBorderCountry(code: string): void {
+    this._countryDService.getCountryDetailsByCode(code).subscribe(data => this.countryData = data);
 
   }
   goBack(): void {
-    this.location.historyGo();
-
+    this._location.back();
   }
 
 
